0f5d7bd5646703d2453e6ae91300a86f45f0b079
BrunoAST <bruno-alves2010@live.com>
2021-12-28 15:29:44 -0300

    feat: add price and description

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -5,8 +5,10 @@ type ProductItemProps = { productItem: ProductItemType };
 const ProductItem: React.FC<ProductItemProps> = (prop: ProductItemProps) => {
   return (
     <div>
-      <span role="contentinfo">{prop.productItem.title}</span>
       <img alt={prop.productItem.title} src={prop.productItem.image} />
+      <span role="contentinfo">{prop.productItem.title}</span>
+      <p>{prop.productItem.description}</p>
+      <span>{prop.productItem.price}</span>
     </div>
   );
 };
