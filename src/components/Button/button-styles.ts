bb6dff048a3f611602b88e115ca029c7f3648ef7
BrunoAST <bruno-alves2010@live.com>
2021-12-28 16:17:31 -0300

    style: add active, hover and focus state for button styles

--- src/components/Button/button-styles.ts
+++ src/components/Button/button-styles.ts
@@ -2,6 +2,7 @@ import { css } from "@emotion/react";
 
 export const button = () => {
     return css`
+    border: none;
     background: #533A7B;
     box-shadow: 0px 8px 25px rgba(0, 0, 0, 0.1);
     border-radius: 15px;
@@ -10,6 +11,14 @@ export const button = () => {
     font-weight: bold;
     padding: 8px 12px;
     cursor: pointer;
+
+    &:active, &:hover {
+        filter: brightness(140%);
+    }
+
+    &:focus {
+        outline: solid lightblue;
+    }
     `
 }
 
