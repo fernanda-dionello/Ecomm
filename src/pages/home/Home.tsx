28dea0c031d9be34ae78a4ec485895b2435df530
BrunoAST <bruno-alves2010@live.com>
2021-12-28 16:18:20 -0300

    style: create style for Home component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,28 @@
+import { css } from "@emotion/css";
+import faker from "faker";
 import React from "react"
+import ProductItem from "../../components/ProductItem/ProductItem";
+import { ProductItemType } from "../../components/ProductItem/types/product-item";
+import { container } from "./home-styles";
 
 const Home: React.FC = () => {
-    return <div>Home</div>
+    const Product: ProductItemType = {
+        id: faker.datatype.number(),
+        category: faker.commerce.productAdjective(),
+        image: faker.image.imageUrl(),
+        title: faker.commerce.productName(),
+        description: faker.commerce.productDescription(),
+        price: faker.datatype.number(),
+    };
+
+    return (
+        <div className={css`${container()}`}>
+            <ProductItem productItem={Product} />
+            <ProductItem productItem={Product} />
+            <ProductItem productItem={Product} />
+            <ProductItem productItem={Product} />
+        </div>
+    );
 }
 
-export default Home
+export default Home;
---
+++ src/pages/home/home-styles.ts
@@ -0,0 +1,11 @@
+import { css } from "@emotion/react"
+
+export const container = () => {
+    return css`
+        gap: 2rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+    `;
+}
