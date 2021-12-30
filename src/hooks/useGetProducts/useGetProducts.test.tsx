3554b2b31735d4f7e32af60645df73a6b1e9ec43
Natalia Kikuchi <nataliakikuchi90@gmail.com>
2021-12-30 15:14:41 -0300

    refactor: changes useGetProducts hook tests

--- src/hooks/useGetProducts/useGetProducts.test.tsx
+++ src/hooks/useGetProducts/useGetProducts.test.tsx
@@ -3,19 +3,18 @@ import useGetProducts from "./useGetProducts";
 import { ProductItemType } from "../../components/ProductItem/types/product-item";
 import faker from "faker";
 import { apiUrl } from "../../api/apiUrl";
-import { act, cleanup, renderHook } from '@testing-library/react-hooks'
+import { act, cleanup, renderHook } from "@testing-library/react-hooks";
 
 describe("useGetProducts hook tests", () => {
-
-  let productList: ProductItemType[] = Array.from({length: 5}).map(() => {
+  let productList: ProductItemType[] = Array.from({ length: 5 }).map(() => {
     return {
-      id: faker.datatype.number({max: 150, min: 50}),
+      id: faker.datatype.number({ max: 150, min: 50 }),
       title: faker.commerce.productName(),
       price: faker.datatype.number(),
       category: faker.commerce.department(),
       description: faker.commerce.productDescription(),
-      image: faker.image.imageUrl()
-    }
+      image: faker.image.imageUrl(),
+    };
   });
 
   beforeEach(() => {
@@ -24,29 +23,25 @@ describe("useGetProducts hook tests", () => {
 
   afterEach(() => {
     cleanup();
-  })
+  });
 
   test("should call the correct router to list the products", async () => {
     const { result } = renderHook(() => useGetProducts());
-    const { getProducts } = result.current;
     fetchMock.mockResponseOnce(JSON.stringify({}));
-    await getProducts();
+    await result.current.getProducts();
 
-    expect(fetch).toHaveBeenCalledWith(
-      `${apiUrl}/products?limit=4`
-    );
+    expect(fetch).toHaveBeenCalledWith(`${apiUrl}/products?limit=4`);
   });
 
   test("should return a list of products", async () => {
     const { result } = renderHook(() => useGetProducts());
-    const { getProducts } = result.current; 
     fetchMock.mockResponseOnce(JSON.stringify(productList));
-    const products = await getProducts();
-    
+    const products = await result.current.getProducts();
+
     expect(products).toEqual(productList);
   });
 
-  test("should update limit of url", async() => {
+  test("should update limit of url", async () => {
     const { result } = renderHook(() => useGetProducts(10));
     fetchMock.mockResponseOnce(JSON.stringify({}));
     act(() => result.current.setLimit(2));
